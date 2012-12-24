dd1898796b8a960f1389377ae18b882a9e0a338b
cubicdaiya <cubicdaiya@gmail.com>
2012-12-24 21:13:15 +0900

    make mrb instance singleton

--- ngx_http_mruby.h
+++ ngx_http_mruby.h
@@ -8,8 +8,16 @@
 #define NGX_HTTP_MRUBY_H
 
 #include <ngx_http.h>
+#include <mruby.h>
 
-ngx_int_t ngx_mrb_run_file(ngx_http_request_t *r, char *code_file);
-ngx_int_t ngx_mrb_run_string(ngx_http_request_t *r, char *code);
+typedef struct ngx_mrb_state_t {
+    mrb_state *mrb;
+    int n;
+    int ai;
+} ngx_mrb_state_t;
+
+ngx_int_t ngx_mrb_run(ngx_http_request_t *r, ngx_mrb_state_t *mrb);
+ngx_int_t ngx_mrb_init_file(char *code_file_path, ngx_mrb_state_t *state);
+ngx_int_t ngx_mrb_init_string(char *code, ngx_mrb_state_t *state);
 
 #endif // NGX_HTTP_MRUBY_H
