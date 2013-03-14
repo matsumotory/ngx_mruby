e8cd0fd41aca1af22dc1bca624a0c8543d73af66
cubicdaiya <cubicdaiya@gmail.com>
2013-03-14 21:34:17 +0900

    divide ngx_http_mruby_module.(c|h) into ngx_http_mruby_(handler|hook).(c|h)

---
+++ ngx_http_mruby_handler.h
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2013 Tatsuhiko Kubo <cubicdaiya@gmail.com>
+ */
+
+#ifndef NGX_HTTP_MRUBY_HANDLER_H
+#define NGX_HTTP_MRUBY_HANDLER_H
+
+#include <nginx.h>
+#include <ngx_http.h>
+
+#include "ngx_http_mruby_core.h"
+#include "ngx_http_mruby_init.h"
+#include "ngx_http_mruby_module.h"
+
+ngx_int_t ngx_http_mruby_post_read_handler(ngx_http_request_t *r);
+ngx_int_t ngx_http_mruby_server_rewrite_handler(ngx_http_request_t *r);
+ngx_int_t ngx_http_mruby_rewrite_handler(ngx_http_request_t *r);
+ngx_int_t ngx_http_mruby_access_handler(ngx_http_request_t *r);
+ngx_int_t ngx_http_mruby_content_handler(ngx_http_request_t *r);
+ngx_int_t ngx_http_mruby_log_handler(ngx_http_request_t *r);
+ngx_int_t ngx_http_mruby_post_read_inline_handler(ngx_http_request_t *r);
+ngx_int_t ngx_http_mruby_server_rewrite_inline_handler(ngx_http_request_t *r);
+ngx_int_t ngx_http_mruby_rewrite_inline_handler(ngx_http_request_t *r);
+ngx_int_t ngx_http_mruby_access_inline_handler(ngx_http_request_t *r);
+ngx_int_t ngx_http_mruby_content_inline_handler(ngx_http_request_t *r);
+ngx_int_t ngx_http_mruby_log_inline_handler(ngx_http_request_t *r);
+
+#endif // NGX_HTTP_MRUBY_HANDLER_H
