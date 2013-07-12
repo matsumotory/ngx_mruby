6095fe7aa68178d9c98b95301474a7d5d2c60611
cubicdaiya <cubicdaiya@gmail.com>
2013-07-13 03:45:44 +0900

    share ngx_mrb_sate_t in each handler

--- src/ngx_http_mruby_module.h
+++ src/ngx_http_mruby_module.h
@@ -19,6 +19,7 @@ typedef struct {
     size_t           size;
     ngx_str_t        script;
     ngx_mrb_state_t *state;
+    ngx_mrb_code_t  *code;
 } ngx_http_mruby_set_var_data_t;
 #include <ndk.h>
 #endif
@@ -28,23 +29,24 @@ typedef struct {
 
 extern ngx_module_t  ngx_http_mruby_module;
 
-typedef struct ngx_http_mruby_loc_conf_t {
-    ngx_mrb_state_t *post_read_state;
-    ngx_mrb_state_t *server_rewrite_state;
-    ngx_mrb_state_t *rewrite_state;
-    ngx_mrb_state_t *access_state;
-    ngx_mrb_state_t *handler_state;
-    ngx_mrb_state_t *log_handler_state;
-    ngx_mrb_state_t *post_read_inline_state;
-    ngx_mrb_state_t *server_rewrite_inline_state;
-    ngx_mrb_state_t *rewrite_inline_state;
-    ngx_mrb_state_t *access_inline_state;
-    ngx_mrb_state_t *content_inline_state;
-    ngx_mrb_state_t *log_inline_state;
-    ngx_flag_t       cached;
-} ngx_http_mruby_loc_conf_t;
+typedef struct ngx_http_mruby_main_conf_t {
+    ngx_mrb_state_t *state;
+} ngx_http_mruby_main_conf_t;
 
-ngx_int_t ngx_mrb_push_ngx_conf(ngx_conf_t *cf);
-ngx_conf_t* ngx_mrb_get_ngx_conf();
+typedef struct ngx_http_mruby_loc_conf_t {
+    ngx_mrb_code_t *post_read_code;
+    ngx_mrb_code_t *server_rewrite_code;
+    ngx_mrb_code_t *rewrite_code;
+    ngx_mrb_code_t *access_code;
+    ngx_mrb_code_t *handler_code;
+    ngx_mrb_code_t *log_handler_code;
+    ngx_mrb_code_t *post_read_inline_code;
+    ngx_mrb_code_t *server_rewrite_inline_code;
+    ngx_mrb_code_t *rewrite_inline_code;
+    ngx_mrb_code_t *access_inline_code;
+    ngx_mrb_code_t *content_inline_code;
+    ngx_mrb_code_t *log_inline_code;
+    ngx_flag_t      cached;
+} ngx_http_mruby_loc_conf_t;
 
 #endif // NGX_HTTP_MRUBY_MODULE_H
--- src/ngx_http_mruby_state.h
+++ src/ngx_http_mruby_state.h
@@ -12,8 +12,12 @@
 #include <mruby/value.h>
 #include "ngx_http_mruby_core.h"
 
-ngx_int_t ngx_http_mruby_state_reinit_from_file(ngx_mrb_state_t *state);
-ngx_mrb_state_t *ngx_http_mruby_mrb_state_from_file(ngx_pool_t *pool, ngx_str_t *value);
-ngx_mrb_state_t *ngx_http_mruby_mrb_state_from_string(ngx_pool_t *pool, ngx_str_t *code);
+ngx_int_t ngx_http_mruby_state_reinit_from_file(ngx_mrb_state_t *state, ngx_mrb_code_t *code);
+ngx_mrb_code_t *ngx_http_mruby_mrb_code_from_file(ngx_pool_t *pool, ngx_str_t *code_file_path);
+ngx_mrb_code_t *ngx_http_mruby_mrb_code_from_string(ngx_pool_t *pool, ngx_str_t *code_s);
+ngx_int_t ngx_http_mruby_shared_state_init(ngx_mrb_state_t *state);
+ngx_int_t ngx_mrb_init_file(ngx_str_t *script_file_path, ngx_mrb_state_t *state, ngx_mrb_code_t *code);
+ngx_int_t ngx_mrb_init_string(ngx_str_t *script, ngx_mrb_state_t *state, ngx_mrb_code_t *code);
+ngx_int_t ngx_http_mruby_shared_state_compile(ngx_mrb_state_t *state, ngx_mrb_code_t *code);
 
 #endif // NGX_HTTP_MRUBY_STATE_H
