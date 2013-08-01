bd3cd4cb984348971425a9bb86092becf4368d63
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2013-08-01 14:32:43 +0900

    Add the method of setting variable for syntax sugar.
    v.set "hoge", v.hoge.to_i * 2
    
    is equal below method:
    
    v.hoge =  v.hoge.to_i * 2

--- src/ngx_http_mruby_var.c
+++ src/ngx_http_mruby_var.c
@@ -111,10 +111,106 @@ static mrb_value ngx_mrb_var_get(mrb_state *mrb, mrb_value self, const char *c_n
     }
 }
 
+static mrb_value ngx_mrb_var_set(mrb_state *mrb, mrb_value self, char *k, mrb_value o, ngx_http_request_t *r)
+{
+    ngx_http_variable_t *v;
+    ngx_http_variable_value_t *vv;
+    ngx_http_core_main_conf_t *cmcf;
+    ngx_str_t key;
+    ngx_uint_t hash;
+    u_char *val;
+
+    if (mrb_type(o) != MRB_TT_STRING) {
+        o = mrb_funcall(mrb, o, "to_s", 0, NULL);
+    }
+
+    val = (u_char *)RSTRING_PTR(o);
+    key.len = strlen(k);
+    key.data = (u_char *)k;
+
+    hash = ngx_hash_strlow(key.data, key.data, key.len);
+    r = ngx_mrb_get_request();
+    cmcf = ngx_http_get_module_main_conf(r, ngx_http_core_module);
+    v = ngx_hash_find(&cmcf->variables_hash, hash, key.data, key.len);
+
+    if (v) {
+        if (!(v->flags & NGX_HTTP_VAR_CHANGEABLE)) {
+            ngx_log_error(NGX_LOG_ERR
+                , r->connection->log
+                , 0
+                , "%s ERROR %s:%d: %s not changeable"
+                , MODULE_NAME
+                , __func__
+                , __LINE__
+                , key.data
+            );
+            goto ARENA_RESTOR_AND_ERROR;
+        } else if (v->set_handler) {
+            vv = ngx_palloc(r->pool, sizeof(ngx_http_variable_value_t));
+            if (vv == NULL) {
+                ngx_log_error(NGX_LOG_ERR
+                    , r->connection->log
+                    , 0
+                    , "%s ERROR %s:%d: memory allocate failed"
+                    , MODULE_NAME
+                    , __func__
+                    , __LINE__
+                );
+                goto ARENA_RESTOR_AND_ERROR;
+            }
+            v->set_handler(r, vv, v->data);
+        } else if (v->flags & NGX_HTTP_VAR_INDEXED) {
+            vv = &r->variables[v->index];
+        } else {
+            ngx_log_error(NGX_LOG_ERR
+                , r->connection->log
+                , 0
+                , "%s ERROR %s:%d: %s is not assinged"
+                , MODULE_NAME
+                , __func__
+                , __LINE__
+                , key.data
+            );
+            goto ARENA_RESTOR_AND_ERROR;
+        }
+        vv->valid = 1;
+        vv->not_found = 0;
+        vv->no_cacheable = 0;
+        vv->data = val;
+        vv->len = (size_t)strlen((char *)val) + 1;
+        ngx_log_error(NGX_LOG_INFO
+            , r->connection->log
+            , 0
+            , "%s INFO %s:%d: set variable key:%s val:%s"
+            , MODULE_NAME
+            , __func__
+            , __LINE__
+            , (char *)key.data
+            , (char *)vv->data
+        );
+        return mrb_str_new(mrb, (char *)vv->data, vv->len);
+    }
+
+    ngx_log_error(NGX_LOG_ERR
+        , r->connection->log
+        , 0
+        , "%s ERROR %s:%d: %s is not found"
+        , MODULE_NAME
+        , __func__
+        , __LINE__
+        , key.data
+    );
+    goto ARENA_RESTOR_AND_ERROR;
+
+ARENA_RESTOR_AND_ERROR:
+    return mrb_nil_value();
+}
+
 static mrb_value ngx_mrb_var_method_missing(mrb_state *mrb, mrb_value self)
 {
     mrb_value name, *a;
-    int alen; mrb_value s_name;
+    int alen, len; 
+    mrb_value s_name;
     char *c_name;
     ngx_http_request_t *r;
 
@@ -128,11 +224,16 @@ static mrb_value ngx_mrb_var_method_missing(mrb_state *mrb, mrb_value self)
     // second get mrb_string with mrb_sym2str
     s_name = mrb_sym2str(mrb, mrb_symbol(name));
     c_name = mrb_str_to_cstr(mrb, s_name);
+    len = strlen(c_name);
 
+  if (c_name[len-1] == '=') {
+    return ngx_mrb_var_set(mrb, self, strtok(c_name, "="), a[0], r);
+  } else {
     return ngx_mrb_var_get(mrb, self, c_name, r);
+  }
 }
 
-static mrb_value ngx_mrb_var_set(mrb_state *mrb, mrb_value self)
+static mrb_value ngx_mrb_var_set_func(mrb_state *mrb, mrb_value self)
 {
     ngx_http_request_t *r;
     ngx_http_variable_t *v;
@@ -237,5 +338,5 @@ void ngx_mrb_var_class_init(mrb_state *mrb, struct RClass *class)
 
     class_var = mrb_define_class_under(mrb, class, "Var", mrb->object_class);
     mrb_define_method(mrb, class_var, "method_missing", ngx_mrb_var_method_missing, ARGS_ANY());
-    mrb_define_method(mrb, class_var, "set", ngx_mrb_var_set, ARGS_REQ(2));
+    mrb_define_method(mrb, class_var, "set", ngx_mrb_var_set_func, ARGS_REQ(2));
 }
