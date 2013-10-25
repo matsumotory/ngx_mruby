cb4d03322d1da560835b252c3d58a3f8cac7c6e5
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2013-10-25 17:00:22 +0900

    Add mruby-io

--- build_config.rb
+++ build_config.rb
@@ -21,6 +21,7 @@ MRuby::Build.new do |conf|
   conf.gem :git => 'git://github.com/iij/mruby-process.git'
   conf.gem :git => 'git://github.com/iij/mruby-pack.git'
   conf.gem :git => 'git://github.com/iij/mruby-digest.git'
+  conf.gem :git => 'git://github.com/iij/mruby-io.git'
   conf.gem :git => 'git://github.com/mattn/mruby-json.git'
   conf.gem :git => 'git://github.com/matsumoto-r/mruby-redis.git'
   conf.gem :git => 'git://github.com/matsumoto-r/mruby-sleep.git'
