fc8e7da904445214ab66fa8686fdb8972dd22fad
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2014-04-19 22:45:04 -0700

    :q:qq:::q

--- test.sh
+++ test.sh
@@ -7,9 +7,10 @@
 
 set -e
 
+. ./nginx_version
+
 NGINX_INSTALL_DIR=`pwd`'/build/nginx'
 NGINX_CONFIG_OPT='--prefix='${NGINX_INSTALL_DIR}
-NGINX_SRC_VER='nginx-1.4.4'
 
 if [ ! -d "./mruby/src" ]; then
     echo "mruby Downloading ..."
