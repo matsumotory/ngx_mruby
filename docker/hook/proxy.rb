e7c58881f0b6dca4352e31e3459ccc6d06b8a77c
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2014-06-15 05:38:51 +0000

    Update docker example for revese proxy

--- docker/hook/proxy.rb
+++ docker/hook/proxy.rb
@@ -15,6 +15,10 @@ unless ENV["PROXY2_PORT_80_TCP_ADDR"].nil?
   backends << ENV["PROXY2_PORT_80_TCP_ADDR"] + ":" + ENV["PROXY2_PORT_80_TCP_PORT"]
 end
 
+unless ENV["PROXY3_PORT_80_TCP_ADDR"].nil?
+  backends << ENV["PROXY3_PORT_80_TCP_ADDR"] + ":" + ENV["PROXY3_PORT_80_TCP_PORT"]
+end
+
 uri = '/mruby-hello'
 
 backends[rand(backends.length)] + uri
