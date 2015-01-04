a9904276ee63efa771f96ac6ab12f2923df83a26
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2015-01-04 11:41:42 +0900

    Add mruby-packe for test

--- test/build_config.rb
+++ test/build_config.rb
@@ -27,6 +27,7 @@ MRuby::Build.new do |conf|
   conf.gem :github => 'mattn/mruby-http'
   conf.gem :github => 'iij/mruby-io'
   conf.gem :github => 'iij/mruby-socket'
+  conf.gem :github => 'iij/mruby-pack'
   # include the default GEMs
   conf.gembox 'default'
 
