29d0fcbd06963a9d1b32f92199df6411e6539fc9
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2015-04-02 18:05:10 +0900

    Bump up version to v1.8.10

--- src/ngx_http_mruby_module.h
+++ src/ngx_http_mruby_module.h
@@ -15,7 +15,7 @@
 #include "ngx_http_mruby_init.h"
 
 #define MODULE_NAME "ngx_mruby"
-#define MODULE_VERSION "1.8.9"
+#define MODULE_VERSION "1.8.10"
 
 typedef enum code_type_t {
   NGX_MRB_CODE_TYPE_FILE,
