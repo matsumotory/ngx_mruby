baeaa4f48d736c3364b144c4b60808009ed4d186
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2015-08-05 23:28:34 +0900

    Bump up version 1.12.14

--- src/ngx_http_mruby_module.h
+++ src/ngx_http_mruby_module.h
@@ -15,7 +15,7 @@
 #include "ngx_http_mruby_init.h"
 
 #define MODULE_NAME "ngx_mruby"
-#define MODULE_VERSION "1.12.13"
+#define MODULE_VERSION "1.12.14"
 
 #if (nginx_version > 1007999)
 #define NGX_USE_MRUBY_UPSTREAM
