65ca577b48239108e56d8c4f3fb7a009163db4e8
MATSUMOTO, Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2016-03-31 00:34:14 +0900

    Update build_config.rb

--- build_config.rb
+++ build_config.rb
@@ -22,7 +22,7 @@ MRuby::Build.new do |conf|
   conf.gem :github => 'matsumoto-r/mruby-userdata'
   conf.gem :github => 'matsumoto-r/mruby-uname'
   conf.gem :github => 'matsumoto-r/mruby-mutex'
-  conf.gem :github => 'matsumoto-r/mruby-cache'
+  conf.gem :github => 'matsumoto-r/mruby-localmemcache'
 
   # ngx_mruby extended class
   conf.gem '../mrbgems/ngx_mruby_mrblib'
