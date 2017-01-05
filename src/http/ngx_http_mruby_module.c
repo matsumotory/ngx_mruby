adafc82b8951e36a85b2727dab250396a780dcbe
MATSUMOTO Ryosuke <matsumotory@pepabo.com>
2017-01-05 17:27:20 +0900

    Forgot to fix mruby_output_body_filter directive

--- src/http/ngx_http_mruby_module.c
+++ src/http/ngx_http_mruby_module.c
@@ -2063,6 +2063,10 @@ static ngx_int_t ngx_http_mruby_body_filter_handler(ngx_http_request_t *r, ngx_c
   ngx_chain_t out;
   ngx_buf_t *b;
 
+  if (ctx->phase == NGX_HTTP_MRUBY_FILTER_PASS) {
+    return ngx_http_next_body_filter(r, in);
+  }
+
   if ((rc = ngx_http_mruby_read_body(r, in, ctx)) != NGX_OK) {
     if (rc == NGX_AGAIN) {
       return NGX_OK;
@@ -2095,15 +2099,30 @@ static ngx_int_t ngx_http_mruby_body_filter_handler(ngx_http_request_t *r, ngx_c
   b->memory = 1;
   b->last_buf = 1;
 
+  r->headers_out.content_length_n = b->last - b->pos;
+
+  if (r->headers_out.content_length) {
+    r->headers_out.content_length->hash = 0;
+  }
+
+  r->headers_out.content_length = NULL;
+
   out.buf = b;
   out.next = NULL;
 
-  r->headers_out.content_length_n = b->last - b->pos;
+  ctx->phase = NGX_HTTP_MRUBY_FILTER_PASS;
+
+  ngx_log_error(NGX_LOG_DEBUG, r->connection->log, 0, "%s DEBUG %s:%d: data after body length: %uz", MODULE_NAME,
+                __func__, __LINE__, ctx->body_length);
+
   rc = ngx_http_next_header_filter(r);
   if (rc == NGX_ERROR || rc > NGX_OK || r->header_only) {
     return NGX_ERROR;
   }
-  return ngx_http_next_body_filter(r, &out);
+
+  rc = ngx_http_next_body_filter(r, &out);
+
+  return rc;
 }
 
 static ngx_int_t ngx_http_mruby_body_filter_inline_handler(ngx_http_request_t *r, ngx_chain_t *in)
