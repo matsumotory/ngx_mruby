83dbf873e9bb7dfd04d041e719ccbffa80f9fa28
MATSUMOTO Ryosuke <matsu1229@gmail.com>
2017-06-30 03:59:56 +0000

    Add error handling which don't use ssl module

--- src/http/ngx_http_mruby_server.c
+++ src/http/ngx_http_mruby_server.c
@@ -79,11 +79,13 @@ static mrb_value ngx_mrb_add_listener(mrb_state *mrb, mrb_value self)
 #if (NGX_HAVE_INET6)
   lsopt.ipv6only = 1;
 #endif
-#if (NGX_HTTP_SSL)
   if (mrb_bool(mrb_hash_get(mrb, listener, mrb_check_intern_cstr(mrb, "ssl")))) {
+#if (NGX_HTTP_SSL)
     lsopt.ssl = 1;
-  }
+#else
+    mrb_raise(mrb, E_RUNTIME_ERROR, "the ssl symbol requires ngx_http_ssl_module");
 #endif
+  }
 
   (void)ngx_sock_ntop(&lsopt.sockaddr.sockaddr, lsopt.socklen, lsopt.addr, NGX_SOCKADDR_STRLEN, 1);
   if (ngx_http_add_listen(cf, cscf, &lsopt) == NGX_OK) {
