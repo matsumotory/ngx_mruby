82b5eb5ea95b5c3c23b5cae48cc3dac786958b9a
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2018-02-19 06:55:32 +0000

    Fix comments

--- mrbgems/ngx_mruby_mrblib/mrblib/mrb_nginx.rb
+++ mrbgems/ngx_mruby_mrblib/mrblib/mrb_nginx.rb
@@ -54,7 +54,7 @@ module Kernel
       nginx_handler.call
     end
     Proc.new do
-      # BUG?: return nginx_handler directly from fiber, not proc.
+      # BUG?: return nginx_handler directly from fiber, not proc in any case.
       result = fiber_handler.resume
       [fiber_handler.alive?, result]
     end
