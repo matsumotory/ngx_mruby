34605ea259fd9a70911422e2bbb4f53125692185
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2018-02-19 06:49:09 +0000

    Bump up version 2.0.0-dev

--- src/http/ngx_http_mruby_module.h
+++ src/http/ngx_http_mruby_module.h
@@ -20,7 +20,7 @@
 #include "ngx_http_mruby_init.h"
 
 #define MODULE_NAME "ngx_mruby"
-#define MODULE_VERSION "1.20.2"
+#define MODULE_VERSION "2.0.0-dev"
 
 #if (nginx_version > 1007999)
 #define NGX_USE_MRUBY_UPSTREAM
