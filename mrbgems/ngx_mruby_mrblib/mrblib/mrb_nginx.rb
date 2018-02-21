0540592fefc51b55696d76586c4c9616ab4d6d17
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2018-02-21 10:18:05 +0000

    Support return on ngx_mruby handler

--- mrbgems/ngx_mruby_mrblib/mrblib/mrb_nginx.rb
+++ mrbgems/ngx_mruby_mrblib/mrblib/mrb_nginx.rb
@@ -50,10 +50,9 @@ module Kernel
   end
 
   def _ngx_mrb_prepare_fiber(nginx_handler)
-    fiber_handler = Fiber.new do
-      nginx_handler.call
-    end
-    Proc.new do
+    fiber_handler = Fiber.new { nginx_handler.call }
+
+    lambda do
       # BUG?: return nginx_handler directly from fiber, not proc in any case.
       result = fiber_handler.resume
       [fiber_handler.alive?, result]
