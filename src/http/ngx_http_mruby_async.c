d9e4141d7ae40b49a9875de17d8d351f965c09ef
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2018-04-02 00:09:29 +0900

    More error handling for output filter of subrequest

--- src/http/ngx_http_mruby_async.c
+++ src/http/ngx_http_mruby_async.c
@@ -265,6 +265,10 @@ static ngx_int_t ngx_http_mruby_read_sub_response(ngx_http_request_t *sr, ngx_ht
 
   p = ctx->sub_response_last;
   out = sr->out;
+  if (out == NULL) {
+    ngx_log_error(NGX_LOG_ERR, sr->connection->log, 0, "%s ERROR %s:%d: http sub request filter NULL output",
+                  MODULE_NAME, __func__, __LINE__);
+  }
 
   for (cl = out; cl != NULL; cl = cl->next) {
     b = cl->buf;
@@ -369,7 +373,7 @@ static mrb_value ngx_mrb_async_http_sub_request(mrb_state *mrb, mrb_value self)
 
   ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "http_sub_request send to %V", actx->uri);
 
-  if (ngx_http_subrequest(r, actx->uri, NULL, &sr, ps, NGX_HTTP_SUBREQUEST_WAITED) !=
+  if (ngx_http_subrequest(r, actx->uri, NULL, &sr, ps, NGX_HTTP_SUBREQUEST_IN_MEMORY | NGX_HTTP_SUBREQUEST_WAITED) !=
       NGX_OK) {
     mrb_raise(mrb, E_RUNTIME_ERROR, "ngx_http_subrequest failed for http_sub_rquest method");
   }
@@ -379,6 +383,7 @@ static mrb_value ngx_mrb_async_http_sub_request(mrb_state *mrb, mrb_value self)
     mrb_raise(mrb, E_RUNTIME_ERROR, "ngx_palloc failed for sr->request_body");
   }
 
+  sr->header_only = 0;
   re->sr = sr;
 
   ctx = ngx_http_get_module_ctx(r, ngx_http_mruby_module);
