3c9c47360e603c725d8e12cafa587b609b184434
pyama86 <pyama@pepabo.com>
2018-05-09 10:37:28 +0900

    fix stack smashing detected

--- src/http/ngx_http_mruby_async.c
+++ src/http/ngx_http_mruby_async.c
@@ -149,7 +149,7 @@ static void ngx_mrb_async_sleep_cleanup(void *data)
 
 static mrb_value ngx_mrb_async_sleep(mrb_state *mrb, mrb_value self)
 {
-  unsigned int timer;
+  mrb_int timer;
   u_char *p;
   ngx_event_t *ev;
   ngx_mrb_reentrant_t *re;
