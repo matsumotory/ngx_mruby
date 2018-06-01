c688dec54a176146782f94b569aa78596ba3d7a8
pyama86 <pyama@pepabo.com>
2018-06-01 12:26:59 +0900

    fix test

--- test/t/ngx_mruby.rb
+++ test/t/ngx_mruby.rb
@@ -658,20 +658,15 @@ if nginx_features.is_async_supported?
 
   t.assert('ngx_mruby - Nginx::Async::HTTP.new "/dst"', 'location /async_http_sub_request') do
     res = HttpRequest.new.get base + '/async_http_sub_request'
-    t.assert_equal '200 OK', res["status"]
-    t.assert_equal 'HTTP/1.1 200 OK', res["header"][0,15]
-    t.assert_equal 'global_ngx_mruby', res["header"][-16,16]
+    t.assert_equal '200', res["code"]
     t.assert_equal '{"query1"=>"foo", "query2"=>"bar"}', res["body"]
 
     res = HttpRequest.new.get base + '/async_http_sub_request_notfound'
-    t.assert_equal '404 Not Found', res["status"]
-    t.assert_equal 'HTTP/1.1 404 Not Found', res["header"][0,22]
+    t.assert_equal '404', res["code"]
     t.assert_equal 'global_ngx_mruby', res["header"][-16,16]
 
     res = HttpRequest.new.get base + '/async_http_sub_request_notfound_ok'
-    t.assert_equal '200 OK', res["status"]
-    t.assert_equal 'HTTP/1.1 200 OK', res["header"][0,15]
-    t.assert_equal 'global_ngx_mruby', res["header"][-16,16]
+    t.assert_equal '200', res["code"]
     t.assert_equal 'ok', res["body"]
   end
 
