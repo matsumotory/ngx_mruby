ae11868bcfa617e7c68dac58a8b9c0b2460e1b9b
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2018-11-15 13:29:02 +0900

    More prepare code formatter and editor for development

--- misc/provision.sh
+++ misc/provision.sh
@@ -4,5 +4,8 @@ sudo apt-get update
 sudo apt-get -y install build-essential rake bison git gperf automake m4 \
                 autoconf libtool cmake pkg-config libcunit1-dev ragel \
                 libpcre3-dev clang-format-6.0
+sudo apt-get -y remove nano
+
+sudo update-alternatives --install /usr/bin/clang-format clang-format /usr/bin/clang-format-6.0 1000
 
 git clone https://github.com/matsumotory/ngx_mruby
