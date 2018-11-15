5f4b3e787ec3c09a92487589b15439519ed8e024
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2018-11-15 13:24:06 +0900

    Install clang-format-6.0 for development

--- misc/provision.sh
+++ misc/provision.sh
@@ -2,6 +2,7 @@
 
 sudo apt-get update
 sudo apt-get -y install build-essential rake bison git gperf automake m4 \
-                autoconf libtool cmake pkg-config libcunit1-dev ragel libpcre3-dev
+                autoconf libtool cmake pkg-config libcunit1-dev ragel \
+                libpcre3-dev clang-format-6.0
 
 git clone https://github.com/matsumotory/ngx_mruby
