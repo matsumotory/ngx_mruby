1215ad238158a80d5d19f2f0170abee9a6c1174d
pyama <pyama@pepabo.com>
2023-09-25 01:41:40 +0900

    string length limit change to 10MB(from 1MB)

--- build_config.rb
+++ build_config.rb
@@ -1,9 +1,9 @@
 MRuby::Build.new('host') do |conf|
-
   toolchain :gcc
 
+  conf.defines << 'MRB_STR_LENGTH_MAX=10485760'
   conf.gembox 'full-core'
-
+  enable_debug
   conf.cc do |cc|
     cc.flags << ENV['NGX_MRUBY_CFLAGS'] if ENV['NGX_MRUBY_CFLAGS']
   end
@@ -16,19 +16,19 @@ MRuby::Build.new('host') do |conf|
   #
   # Recommended for ngx_mruby
   #
-  conf.gem :github => 'iij/mruby-env'
-  conf.gem :github => 'iij/mruby-dir'
-  conf.gem :github => 'iij/mruby-digest'
-  conf.gem :github => 'iij/mruby-process'
-  conf.gem :github => 'mattn/mruby-json'
-  conf.gem :github => 'mattn/mruby-onig-regexp'
-  conf.gem :github => 'matsumotory/mruby-redis'
-  conf.gem :github => 'matsumotory/mruby-vedis'
-  conf.gem :github => 'matsumotory/mruby-userdata'
-  conf.gem :github => 'matsumotory/mruby-uname'
-  conf.gem :github => 'matsumotory/mruby-mutex'
-  conf.gem :github => 'matsumotory/mruby-localmemcache'
-  conf.gem :mgem => 'mruby-secure-random'
+  conf.gem github: 'iij/mruby-env'
+  conf.gem github: 'iij/mruby-dir'
+  conf.gem github: 'iij/mruby-digest'
+  conf.gem github: 'iij/mruby-process'
+  conf.gem github: 'mattn/mruby-json'
+  conf.gem github: 'mattn/mruby-onig-regexp'
+  conf.gem github: 'matsumotory/mruby-redis'
+  conf.gem github: 'matsumotory/mruby-vedis'
+  conf.gem github: 'matsumotory/mruby-userdata'
+  conf.gem github: 'matsumotory/mruby-uname'
+  conf.gem github: 'matsumotory/mruby-mutex'
+  conf.gem github: 'matsumotory/mruby-localmemcache'
+  conf.gem mgem: 'mruby-secure-random'
 
   # ngx_mruby extended class
   conf.gem './mrbgems/ngx_mruby_mrblib'
@@ -45,7 +45,7 @@ MRuby::Build.new('host') do |conf|
   # conf.gem :github => 'matsumotory/mruby-discount'
 
   # use mysql on ngx_mruby
-  #conf.gem :github => 'mattn/mruby-mysql'
+  # conf.gem :github => 'mattn/mruby-mysql'
 
   # have GeoIPCity.dat
   # conf.gem :github => 'matsumotory/mruby-geoip'
@@ -53,12 +53,12 @@ MRuby::Build.new('host') do |conf|
   # Linux only for ngx_mruby
   # conf.gem :github => 'matsumotory/mruby-capability'
   # conf.gem :github => 'matsumotory/mruby-cgroup'
-
 end
 
 MRuby::Build.new('test') do |conf|
   # load specific toolchain settings
 
+  conf.defines << 'MRB_STR_LENGTH_MAX=10485760'
   # Gets set by the VS command prompts.
   if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
     toolchain :visualcpp
@@ -68,13 +68,13 @@ MRuby::Build.new('test') do |conf|
 
   enable_debug
 
-  conf.gem :github => 'matsumotory/mruby-simplehttp'
-  conf.gem :github => 'matsumotory/mruby-httprequest'
-  conf.gem :github => 'matsumotory/mruby-uname'
-  conf.gem :github => 'matsumotory/mruby-simpletest'
-  conf.gem :github => 'mattn/mruby-http'
-  conf.gem :github => 'mattn/mruby-json'
-  conf.gem :github => 'iij/mruby-env'
+  conf.gem github: 'matsumotory/mruby-simplehttp'
+  conf.gem github: 'matsumotory/mruby-httprequest'
+  conf.gem github: 'matsumotory/mruby-uname'
+  conf.gem github: 'matsumotory/mruby-simpletest'
+  conf.gem github: 'mattn/mruby-http'
+  conf.gem github: 'mattn/mruby-json'
+  conf.gem github: 'iij/mruby-env'
 
   # include the default GEMs
   conf.gembox 'full-core'
